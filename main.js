a82a24241d451bc55b87d22d5814c102dae94bca
ReflectMun <wksh229@gmail.com>
2022-01-26 08:53:26 +0900

    최초 커밋

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ import express from 'express'
 const app = express()
 const port = 14450
 
+app.get('/', (req, res) => {
+    const response = {
+        code: 200,
+        body: 'Hello, World!'
+    }
+
+    res.json(response)
+})
+
 app.listen(port, () => {
     console.log('Server start')
 })
