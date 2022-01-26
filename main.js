4ca6e806c7f8a21c966af2b81f2cd7587729d68f
ReflectMun <wksh229@gmail.com>
2022-01-26 11:56:28 +0900

    수정

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import express from 'express'
 const app = express()
 const port = 14450
 
+import login from './router/login.js'
+
+app.use('/login', login)
+
 app.get('/', (req, res) => {
     const response = {
         code: 200,
--- router/login.js
+++ router/login.js
@@ -4,7 +4,7 @@ import { createPool } from 'mysql2/promise'
 const login = Router()
 
 login.get('/', (req, res) => {
-    
+    res.send('로그인 화면')
 })
 
 export default login
