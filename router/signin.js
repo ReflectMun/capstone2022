63127161e21385028d1f17a4d46612a56342a1ba
ReflectMun <wksh229@gmail.com>
2022-01-29 14:59:13 +0900

    회원가입 기능 구현 시작

--- router/signin.js
+++ router/signin.js
@@ -3,6 +3,11 @@ import { Router } from 'express'
 const signin = Router()
 
 signin.get('/', (req, res) => {
+    const ID = req.body['ID']
+    const password = req.body['password']
+    const nickName = req.body['nickName']
+    const eMail = req.body['eMail']
+
     
 })
 
