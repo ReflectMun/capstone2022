afb632f021c449c60b7c0f80b7e8f819c734b1ae
ReflectMun <wksh229@gmail.com>
2022-02-07 16:22:42 +0900

    d

--- router/test/test.js
+++ router/test/test.js
@@ -3,20 +3,19 @@ import { readFile } from 'fs'
 import AWS from 'aws-sdk'
 
 const test = Router()
+
+// 로컬에서 테스트 시엔 이 코드를 주석 해제하고 테스트 해보시면 됩니다
+// 주석 설정 단축키 : ctrl(command) + /
 const S3 = new AWS.S3({
     accessKeyId: 'AKIAY6X2UAVIUFOCEBEJ',
     secretAccessKey: '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',
     region: 'us-east-1'
 })
 
-const bucketName = 'capsotnestorage'
+// 서버로 푸시할 땐 반드시 이거 주석 해제하시고, 위 코드는 주석 처리 후 푸시해주세요
+// const S3 = new AWS.S3()
 
-S3.listBuckets((err, data) => {
-    if(err) { console.log(err) }      
-    else{
-        console.log(data)
-    }
-})
+const bucketName = 'capsotnestorage'
 
 test.get('/', (req, res) => {
     readFile('public/html/test.html', { encoding: 'utf-8' }, (err, data) => {
