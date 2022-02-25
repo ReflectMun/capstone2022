9c147ef7c0c1e4591ce8ce3f5ecdb15bc05d3526
ReflectMun <wksh229@gmail.com>
2022-02-25 14:12:57 +0900

    dd

--- router/test/test.js
+++ router/test/test.js
@@ -1,13 +1,39 @@
 import { Router } from 'express'
 import { readFile } from 'fs'
+import AWS from 'aws-sdk'
 
 const test = Router()
 
-test.get('/', show)
+test.get('/', (req, res) => {
+    readFile('./public/html/test/test.html', { encoding: 'utf-8' }, (err, data) => {
+        if(err) { res.send('404 Not Found') }
+        else{
+            res.writeHead(200, {
+                'Content-Type': 'text/html; encoding=utf-8'
+            })
+            res.write(data)
+            res.end()
+        }
+    })
+})
 
-function show(req, res, next){
-    res.send('Hello, World!')
-}
+test.get('/s3Image', (req, res) => {
+    const S3 = new AWS.S3({
+        accessKeyId: 'AKIAY6X2UAVITBIQ7V4B',
+        secretAccessKey: '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',
+        region: 'ap-northeast-2'
+    })
+
+    S3.getObject({
+        Bucket: 'capstonestorage',
+        Key: 'Fox.jpeg'
+    }, (err, data) => {
+        if(err) { console.log(`S3 Bucket에 파일이 존재하지 않음`) }
+        else{
+            res.send(data.Body)
+        }
+    })
+})
 
 test.get('/param/:num', param)
 
