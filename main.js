a571cf82cf387e77291e9ed7b71b7b789a2284c3
ReflectMun <wksh229@gmail.com>
2022-05-18 17:41:28 +0900

    d

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'localhost:3000'
+    origin: 'http://localhost:3000'
 }))
 
 app.use(serveStatic(join(__dirname, 'public/html')))
