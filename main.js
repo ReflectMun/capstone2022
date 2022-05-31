18794f5112f480aa13a0724cf4497fabc6b46298
ReflectMun <wksh229@gmail.com>
2022-06-01 01:40:09 +0900

    cors

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "'*'"
+    origin: '*'
 }))
 
 app.use(serveStatic(join(__dirname, 'public/html')))
