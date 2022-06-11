66f8bd8c975da1db5820f1d2b04dedab47b2a201
ReflectMun <wksh229@gmail.com>
2022-06-11 23:02:24 +0900

    point

--- router/point.js
+++ router/point.js
@@ -85,6 +85,46 @@ export async function checkEnoughPoint(req, res, next){
         res.json({ code: 5051, message: '포인트량을 처리하는 도중 오류가 발생하였습니다' })
     }
 }
+
+/**
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next 
+ */
+ export async function increaseUserPoint(req, res, next){
+    const { UID } = req.paramBox
+    const pointAdd = 5
+    let conn
+
+    try{
+        const queryString = `SELECT Point FROM Users WHERE UID = ${UID}`
+        conn = await Pool.getConnection()
+
+        await conn.beginTransaction()
+        const [ data, fields ] = await conn.query(queryString)
+        await conn.commit()
+
+        const point = data[0]['Point']
+        if(pointAdd > point){
+            res.json({ code: 5500, msg: "포인트가 부족합니다"})
+        }else{
+            const queryString2 = `UPDATE Users SET Point=${point + pointAdd} WHERE UID = ${UID}`
+            await conn.query(queryString2)
+            await conn.commit()
+
+            conn.release()
+            next()
+        }
+    }
+    catch(err){
+        if(conn){
+            await conn.rollback()
+            conn.release()
+        }
+        errorLog(req, controllerName, err.message += '=3')
+        res.json({ code: 5051, message: '포인트량을 처리하는 도중 오류가 발생하였습니다' })
+    }
+}
 /////////////////////////////////////////////////////
 
 export default fetchPoint
